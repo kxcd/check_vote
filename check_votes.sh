eff98377173f2795a77d11c20abefd41d95a5087
Michael <xkcd@mpx.com.au>
2024-02-16 18:52:28 +1100

    Evonode support
    * Bug fixes.
    * Adds `-debug` option for more output.
    * Supports evonodes.

--- check_votes.sh
+++ check_votes.sh
@@ -3,7 +3,8 @@
 set -e
 
 # An array of protx hashes.
-MASTERNODES=(bc5ab1d9d0ef562563194b1eb594c248919d014b106b0d3759fc3fd577da2054 7166801342a2f09358ae4d3720f912c439605db9f58af362e9eccd44e8509fbb e8ce80ea903a5f4af70dfed189fbcf6926374743202c4cedafe58c21eafdc51b a569acf816ff49a72c18a62bdcc59bb95ed962c9c6ceae2fb4006e30d1946700 65650b10d273b74db238649477bc24222b4171cc1bbb8e0bd414d0eedee76f4f f134927ab6aeed105d1ac93ca3b9d2f197831abad91540e125aec03e8ae33437 7afd763df1b77ac1e9fa2a43d7387142fba5bb10e89462b16c9d1d14347d4c4c f1916d329797bb25ad4062e193473b9b48c344c10b48dc1bd5ee5ebb6b499a0a)
+MASTERNODES=(0062e548ac39d518de7b74b9ea92cf6735a8699a3d70896e533dbb5167aedd0b 0092aa49ee56297a47a5ee6dccca746b58a69f1bf9a24e3e28d9c1ea9bae41ea 00a60e606dd7724391d9f8ee9245b03144921149d6c294439ee81ccdd1de9d2d 00a6aa2d8bc371d4577c887a5c68003b75dba238eebc9c6d76941b1e7b7a4304 fef139ff6fc509529dad0540a48d5b305dc3eb319c7de3afb3ff63f52d838d95 ff00b58e4b281f41d6d87df335ab8e632177c3c2ec57c6e4e0d7f248e0bb9f51 ff09ea3a7ef93a0bf5179cec8eee1271a15afc702902b5d6f5a38331e206d3b0 ff93208c66329768323a47711f7e8c211c8b42dd38b8a2dc36118d1d70fe673e ff93af84ac3f7cd70dc522696e9d012662e00fff403ff78f122e6dbd7a6a4b54 ffa4a74c58aa0002696df1a7d2562b5a36f5116581a0df1a91500a9664985f52 ffa516477af8d6a3cea068a625401415934585f71e6fad72c880ed157da95f92 ffaaf68b98d79e28fd26f9404696385f64d3576af39507adfc295e8037993787)
+
 
 txtred='\e[0;31m' # Red
 txtgrn='\e[0;32m' # Green
@@ -22,14 +23,17 @@ bldwht='\e[1;37m' # White
 
 txtrst='\e[0m'    # Text Reset
 
+[[ $1 == "-debug" ]]&&debug=yes
 
 COLLATERALS=()
+TYPES=()
 
 # Create the array of collaterals via the protx lookup.
 for (( i=0; i < "${#MASTERNODES[*]}"; i++ ));do
 	protx_info=$(dash-cli protx info "${MASTERNODES[$i]}")
 	collateral=$(jq -r '"\(.collateralHash)-\(.collateralIndex)"' <<< "$protx_info")
 	COLLATERALS[$i]=$collateral
+	TYPES[$i]=$(jq -r ".type"<<<"$protx_info")
 done
 
 #echo "${COLLATERALS[@]}"
@@ -38,12 +42,12 @@ done
 # Find all the proposal names and hashes.
 declare -A PROPOSALS
 
-gobject=$(dash-cli gobject list)
+gobject=$(dash-cli gobject list all proposals)
 for hash in $(jq -r '.[].Hash' <<<"$gobject");do
 	dataHex=$(jq -r ".\"$hash\".DataHex"<<<"$gobject")
 	dataString=$(dash-cli gobject deserialize "$dataHex")
 	objectType=$(jq -r '.type'<<<"$dataString")
-	if((objectType==2));then continue;fi
+	((objectType==2))&&continue
 	name=$(jq -r '.name'<<<"$dataString")
 	PROPOSALS[$hash]="$name"
 done
@@ -58,15 +62,19 @@ for prop_hash in "${!PROPOSALS[@]}";do
 done
 #echo "Length: $prop_name_length"
 
+
+[[ -n $debug ]]&&((prop_name_length+=64))
+
 # Print the header.
 for((i=0;i<prop_name_length;i++));do
 	header+=" "
 done
 echo -e "$header  M A S T E R N O D E S\n"
 for((i=0;i<${#MASTERNODES[@]};i++));do
-	header+="  $((i+1))"
+#	header+="  $((i+1))"
+	[[ "${TYPES[$i]}" == "Evo" ]]&&header+="  ${bldwht}$((i+1))${txtrst}"||header+="  $((i+1))"
 done
-echo "$header"
+echo -e "$header"
 for((i=0;i<${#header};i++));do
 	spacer+="-"
 done
@@ -75,6 +83,7 @@ echo "$spacer"
 # How we voted.  I am sorting the proposals on value first.
 for prop_hash in $(for key in "${!PROPOSALS[@]}"; do echo "$key ${PROPOSALS[$key]}"; done|sort -k2|awk '{print $1}');do
 	row="${PROPOSALS[$prop_hash]}"
+	[[ -n $debug ]]&&row="$prop_hash $row"
 	while((${#row}<prop_name_length));do
 		row+=" "
 	done
@@ -110,5 +119,7 @@ echo -e "\nLegend\n"
 padding=" "
 for((i=0;i<${#MASTERNODES[@]};i++));do
 	((i==9))&&padding=
-	echo "$((i+1))${padding} - ${MASTERNODES[$i]}"
+	echo "$((i+1))${padding} - ${MASTERNODES[$i]} ${TYPES[$i]}"
 done
+
+
